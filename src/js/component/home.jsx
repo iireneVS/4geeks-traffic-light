7e246cad45c410e103711bf26fec226766624509
Irene VS <147383929+iireneVS@users.noreply.github.com>
2024-01-12 20:03:07 +0000

    feat: traffic-bonus

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+
+export function Home() {
+	const [selectedColor, setSelectedColor] = useState("yellow");
+	const [showPurple, setShowPurple] = useState(false);
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="traffic-light text-center">
+			<div
+				onClick={() => {
+					setSelectedColor("red");
+					setShowPurple(false); // Oculta el disco morado al cambiar de color
+				}}
+				className={"light red" + (selectedColor === "red" ? " glow1" : "")}
+			></div>
+
+			<div
+				onClick={() => {
+					setSelectedColor("yellow");
+					setShowPurple(false); // Oculta el disco morado al cambiar de color
+				}}
+				className={"light yellow" + (selectedColor === "yellow" ? " glow2" : "")}
+			></div>
+
+			<div
+				onClick={() => {
+					setSelectedColor("green");
+					setShowPurple(false); // Oculta el disco morado al cambiar de color
+				}}
+				className={"light green" + (selectedColor === "green" ? " glow3" : "")}
+			></div>
+
+			
+			<button type="button" class="btn btn-secondary" onClick={() => setShowPurple(!showPurple)}>Botón</button>
+
+			{showPurple && (
+				<div
+					onClick={() => {
+						setSelectedColor("purple");
+						setShowPurple(true);
+					}}
+					className={"light purple" + (selectedColor === "purple" ? " glow4" : "")}
+				></div>
+			)}
 		</div>
+		
 	);
-};
+
+}
 
 export default Home;
